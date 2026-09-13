e19cb91a9156404ba31be2fcfa1c67092c5ef089
Harshit Sharma <jagratisharma553@gmail.com>
2026-09-13 13:45:39 +0530

    Time: 14 ms (89.3%), Space: 47.9 MB (5.75%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -21,6 +21,7 @@ q.add(new int []{i,j});
       int [][]directions={
         {-1,0},{1,0},{0,-1},{0,1}
       } ;
+      int max=0;
       while(!q.isEmpty()){
         int []curr=q.poll();
         int r=curr[0];
@@ -30,23 +31,11 @@ q.add(new int []{i,j});
             int nc=c+dir[1];
             if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==-1){
                 grid[nr][nc]=grid[r][c]+1;
+                   max = Math.max(max, grid[nr][nc]);
                 q.add(new int[]{nr,nc});
             }
         }
       } 
-      int max=findmax(grid);
       return max;
-    }
-    public int findmax(int[][]grid){
-        int n=grid.length;
-        int m=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(m<grid[i][j]){
-                    m=grid[i][j];
-                }
-            }
-        }
-        return m;
     }
 }
