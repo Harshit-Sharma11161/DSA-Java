2198bdf71b655cfb8292f35f2a7cf0883dc339a3
Harshit Sharma <jagratisharma553@gmail.com>
2026-09-22 12:21:48 +0530

    Time: 3 ms (89.85%), Space: 83.3 MB (47.89%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxJump(int[] stones) {
+       int cost=stones[1]-stones[0];
+       for(int i=2;i<stones.length;i++){
+        cost=Math.max(cost,stones[i]-stones[i-2]);
+       } 
+       return cost;
+    }
+}
