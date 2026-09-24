3677dd2ff877cdf1fc7a72ac93179b8dec0e04d8
Harshit Sharma <jagratisharma553@gmail.com>
2026-09-24 06:00:40 +0530

    Time: 4 ms (91.66%), Space: 47.3 MB (21.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
-            Stack<Integer>st=new Stack<>();
         ArrayList<List<Integer>>adj=new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
@@ -13,18 +12,18 @@ class Solution {
         }
         for(int i=0;i<V;i++){
             if(state[i]==0){
-                if(dfs(i,state,adj,st)){
+                if(dfs(i,state,adj)){
                  return false;  
                 }
             }
         }
         return true;
     }
-  public boolean  dfs(int i,int []state,ArrayList<List<Integer>>adj,Stack<Integer>st){
+  public boolean  dfs(int i,int []state,ArrayList<List<Integer>>adj){
       state[i]=1;
       for(int nbr:adj.get(i)){
           if(state[nbr]==0){
-              if(dfs(nbr,state,adj,st)==true)
+              if(dfs(nbr,state,adj))
               return true;
           }
           else if(state[nbr]==1){
